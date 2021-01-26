a1d23ec1a050902f8839055db9ff1fb1625ef123
bigrockquiz <robert.sundin1996@gmail.com>
2021-01-26 10:53:52 +0100

    Update

--- App.js
+++ App.js
@@ -1,21 +1,65 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View, Image } from 'react-native';
+import { NavigationContainer, useNavigation, useRoute } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScreenNavigator />
   );
 }
+
+const ScreenNavigator = () => {
+  const Stack = createStackNavigator();
+  return(
+    <NavigationContainer>
+      <ScreenNavigator>
+        <Stack.Screen name= "HomeScreen" component={HomeScreen}/>
+        <Stack.Screen name= "CategoryScreen" component={CategoryScreen}/>
+      </ScreenNavigator>
+    </NavigationContainer>
+  )
+}
+
+const CategoryScreen = () => {
+  const route = useRoute()
+  return(
+    <View style={styles.header}>
+      <Text style={styles.header}>Choose Category</Text>
+      <Button title="Sport" 
+      onPress={() => console.log('Play pressed')}/>
+      <Button title="Movies" 
+      onPress={() => console.log('Play pressed')}/>
+    </View>
+  )
+}
+
+const HomeScreen = () => {
+  const route = useRoute()
+  const navigation = useNavigation();
+  return(
+    <View style={styles.header}>
+      <Text style={styles.header}>50/50 Game</Text>
+      <Button title="Play" 
+      onPress={() => navigation.navigate("CategoryScreen")}/>
+    </View>
+  )
+}
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'blue',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  header: {
+    alignItems: 'center',
+    fontSize: 40,
+    fontWeight: 'bold',
+    padding: 10,
+    color: 'green'
   },
 });
---
+++ example.js
@@ -0,0 +1,15 @@
+import {Button, Text, View} from 'react-native';
+import React, {useState} from 'react';
+
+
+export function BackgroundColorExample () {
+
+return (
+<View style = {{width: '100%', height: '50%', backgroundColor: 'grey'}}>
+    <Text>Hello World</Text>
+</View>
+
+    );
+}
+
+
